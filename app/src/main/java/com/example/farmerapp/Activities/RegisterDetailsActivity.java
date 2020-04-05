0bb30042a07534dabf292dda14c9c5890857be3a
Yash <yashgupta417@gmail.com>
2020-04-05 20:38:18 +0530

    added LocationUtil class

--- app/src/main/java/com/example/farmerapp/Activities/RegisterDetailsActivity.java
+++ app/src/main/java/com/example/farmerapp/Activities/RegisterDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.example.farmerapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,13 +13,15 @@ import com.example.farmerapp.ViewPager.RegistrationViewPager;
 
 public class RegisterDetailsActivity extends AppCompatActivity {
     RegisterDetailsAdapter mAdapter;
-    static RegistrationViewPager vPager;
+    public static RegistrationViewPager vPager;
+    public static ConstraintLayout parent;
     public static Uri image;
     public static Farmer farmer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_details);
+        parent=findViewById(R.id.registration_parent);
         farmer = new Farmer();
         vPager = findViewById(R.id.regTabs);
         vPager.disableScroll(true);
--- app/src/main/java/com/example/farmerapp/Fragments/RegisterAddress1Fragment.java
+++ app/src/main/java/com/example/farmerapp/Fragments/RegisterAddress1Fragment.java
@@ -2,6 +2,8 @@ package com.example.farmerapp.Fragments;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -12,10 +14,14 @@ import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 
+import android.provider.Settings;
 import android.text.Editable;
+import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -25,10 +31,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.farmerapp.Activities.RegisterDetailsActivity;
 import com.example.farmerapp.R;
+import com.example.farmerapp.Utils.LocationUtil;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.io.IOException;
 import java.util.List;
@@ -36,18 +45,15 @@ import java.util.Locale;
 
 public class RegisterAddress1Fragment extends Fragment {
 
-    LocationManager locationMangaer = null;
-    LocationListener locationListener = null;
-    Location location;
-    private static final long MIN_DISTANCE_FOR_UPDATE = 10;
-    private static final long MIN_TIME_FOR_UPDATE = 1000 * 60 * 2;
+
 
     EditText address1, address2, landmark;
     ImageView back;
     LinearLayout locationLL;
     Button next;
     Boolean isAddress1Ok = false, isAddress2OK = false;
-
+    TextView locationTextView;
+    Integer locationStatus=0;
     public RegisterAddress1Fragment() {
         // Required empty public constructor
     }
@@ -61,44 +67,13 @@ public class RegisterAddress1Fragment extends Fragment {
         locationLL = v.findViewById(R.id.location_ll);
         back = v.findViewById(R.id.back);
         next = v.findViewById(R.id.next);
+        locationTextView=v.findViewById(R.id.location_textview);
         addTextListeners();
         addButtonClickListeners();
-        addLocationListener();
         return v;
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 10) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                getCurrentAddress();
-            }
-        }
-    }
-    public void addLocationListener(){
-        locationListener=new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
 
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
-    }
     public void addTextListeners() {
         address1.addTextChangedListener(new TextWatcher() {
             @Override
@@ -161,7 +136,6 @@ public class RegisterAddress1Fragment extends Fragment {
             @Override
             public void onClick(View v) {
                 getPermissionAndGetAddress();//it will get pin,state,city as well and will be saved but address2 will not be skipped
-                updateNextButtonStatus();
             }
         });
     }
@@ -177,44 +151,56 @@ public class RegisterAddress1Fragment extends Fragment {
             next.setAlpha(0.3f);
         }
     }
-
-    public void getPermissionAndGetAddress() {
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (getContext().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && getContext().checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 10);
-            } else {
-                getCurrentAddress();
-            }
-        } else {
-            getCurrentAddress();
-        }
-    }
-
-    public void getCurrentAddress() {
-        locationMangaer = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+    public void getAddress(){
+        LocationUtil locationUtil=new LocationUtil(getActivity().getApplication());
+        if(!locationUtil.isLocationEnabled(getContext())){
+            Snackbar snackbar=Snackbar.make(RegisterDetailsActivity.parent,"Please turn your GPS on.",Snackbar.LENGTH_SHORT);
+            snackbar.show();
             return;
         }
-        locationMangaer.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_FOR_UPDATE, MIN_DISTANCE_FOR_UPDATE, locationListener);
-        location = locationMangaer.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        Geocoder geocoder = new Geocoder(getContext(),Locale.getDefault());
-        List<Address> addresses= null;
-        try {
-            addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(), 1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String add1 = addresses.get(0).getAddressLine(0);
-        String add2 = addresses.size()>1?addresses.get(0).getAddressLine(1):"";
-        String locality = addresses.get(0).getLocality();
-        String subLocality = addresses.get(0).getSubLocality();
-        String state = addresses.get(0).getAdminArea();
-        String postalCode = addresses.get(0).getPostalCode();
+        if(locationStatus==1)
+            return;//location already fetched
+        locationTextView.setText("Locating....");
+        locationUtil.observeAddress().observe(this, new Observer<List<Address>>() {
+            @Override
+            public void onChanged(List<Address> addresses) {
+                String add1 = addresses.get(0).getAddressLine(0);
+                String add2 = addresses.size() > 1 ? addresses.get(0).getAddressLine(1) : "";
+                String locality = addresses.get(0).getLocality();
+                String subLocality = addresses.get(0).getSubLocality();
+                String state = addresses.get(0).getAdminArea();
+                String postalCode = addresses.get(0).getPostalCode();
+                updateAddressInUI(add1,subLocality,state,postalCode);
+                locationStatus=1;
+            }
+        });
 
-        updateAddressInUI(add1,add2);
     }
-    public void updateAddressInUI(String add1,String add2){
+    public void updateAddressInUI(String add1,String subLocality,String state,String postalCode){
         address1.setText(add1);
-        address2.setText(add2);
+        address2.setText(subLocality+" "+state+" "+postalCode);
+        locationTextView.setText(subLocality+","+state);
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 10) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                getAddress();
+            }
+        }
+    }
+
+    private void getPermissionAndGetAddress() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (getActivity().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && getActivity().checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 10);
+            } else {
+                getAddress();
+            }
+        } else {
+            getAddress();
+        }
+    }
+
 }
