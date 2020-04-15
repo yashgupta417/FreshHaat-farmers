13ab9f71d5b9f17e3ce1c8f2a44614666d2ad86d
Yash <yashgupta417@gmail.com>
2020-04-15 11:12:19 +0530

    night mode sorted

--- app/src/main/java/com/example/farmerapp/Activities/SplashActivity.java
+++ app/src/main/java/com/example/farmerapp/Activities/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.example.farmerapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Context;
 import android.content.Intent;
@@ -19,6 +20,7 @@ public class SplashActivity extends AppCompatActivity {
     public static String MOBILE_NO="mobile_number";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         new Handler().postDelayed(new Runnable(){
