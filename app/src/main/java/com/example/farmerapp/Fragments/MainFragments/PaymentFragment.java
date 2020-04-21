c908cd59e74b1ec8d618acf48365cbd964377a75
Yash <yashgupta417@gmail.com>
2020-04-21 13:01:40 +0530

    payment screen added

--- app/src/main/java/com/example/farmerapp/Fragments/MainFragments/PaymentFragment.java
+++ app/src/main/java/com/example/farmerapp/Fragments/MainFragments/PaymentFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.farmerapp.Activities.MainActivity;
 import com.example.farmerapp.R;
 public class PaymentFragment extends Fragment {
     public PaymentFragment() {
@@ -15,6 +16,9 @@ public class PaymentFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_payment, container, false);
+        View v =inflater.inflate(R.layout.fragment_payment, container, false);
+        MainActivity.hideLocation();
+        MainActivity.setTitle("Payments");
+        return v;
     }
 }
